ae18f917c6d1613ed608369dcdf4a463146be0fe
Exilliar <rorywebber1000@gmail.com>
2021-11-03 17:02:42 +0000

    #1 added vuetify and header from wireframe

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app-bar
+      app
+      color="white"
+    >
+      <v-toolbar-title class="d-flex align-center">
+          <!-- div to be replaced with svg of logo -->
+        <div class="logo">CAP</div>
+
+        Carbon Assessment Platform
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div v-if="loggedIn" class="d-flex align-center">
+          <v-btn color="primary" text>About</v-btn>
+          <v-btn color="primary" text>Help</v-btn>
+          <v-btn color="primary" text class="mr-2">Log Out</v-btn>
+          <v-btn color="primary" text>New Assessment</v-btn>
+      </div>
+    </v-app-bar>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class Header extends Vue {
+    @Prop() loggedIn!: boolean;
+}
+</script>
+<style scoped>
+.logo {
+    color: #CC0A0A;
+    font-size: 1.5rem;
+    padding-right: 0.75rem;
+}
+</style>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#CC0A0A',
+                secondary: '#ffffff'
+            }
+        }
+    }
+});

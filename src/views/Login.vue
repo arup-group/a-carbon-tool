282c2023997f1a0af11f187497661e9489c0f530
Exilliar <43271984+Exilliar@users.noreply.github.com>
2021-11-04 13:01:33 +0000

    #1 added vuetify and header from wireframe (#2)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  components: { HelloWorld }
+})
+export default class Login extends Vue {
+
+}
 </script>

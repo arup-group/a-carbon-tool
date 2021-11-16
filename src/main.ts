0a45d03d2f01b9a6db7cc3f9c72b8b2749775f1e
Exilliar <rorywebber1000@gmail.com>
2021-11-16 16:37:32 +0000

    #9 updated theme, added logo

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 import vuetify from './plugins/vuetify'
 
+import "@/assets/style.css";
+
 Vue.config.productionTip = false;
 
 new Vue({

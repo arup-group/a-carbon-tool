8b8fc1d232cec39d3f45646c88ec451707dd7422
spsarras <psarras.st@gmail.com>
2021-11-17 17:36:22 +0000

    code pieces and yml files for building infrastructure and deploying to that bucket

---
+++ infra/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+
+terraform {
+  backend "s3" {
+    encrypt = true
+  }
+}
+
+module "static_site" {
+  source = "git::https://github.com/arup-group/tf-aws-internal-static-pages.git"
+  domain = var.domain
+  subdomain = "act"
+  azure_app_client_secret = var.azure_app_client_secret
+  azure_app_client_id = var.azure_app_client_id
+  private_key = var.private_key
+  public_key = var.public_key
+  redirect_uri = var.redirect_uri
+  environment = var.environment
+  project_name = "a-carbon-tool"
+}

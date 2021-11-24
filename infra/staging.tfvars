193cd6933bea8f3efd60ed08ba63e386c0879d79
Stamatios Psarras <psarras.st@gmail.com>
2021-11-24 15:31:16 +0000

    Actions for publishing (#16)
    * Create node.js.yml (#3)
    
    * #1 added vuetify and header from wireframe (#2)
    
    * Create node.js.yml
    
    * Update node.js.yml
    
    * changed to npm run test:unit
    
    * removed not working test
    
    * also on dev
    
    Co-authored-by: Exilliar <43271984+Exilliar@users.noreply.github.com>
    
    * #9 updated theme, added logo (#10)
    
    * Deploy to AWS (#11)
    
    * Create node.js.yml (#3) (#6)
    
    * #1 added vuetify and header from wireframe (#2)
    
    * Create node.js.yml
    
    * Update node.js.yml
    
    * changed to npm run test:unit
    
    * removed not working test
    
    * also on dev
    
    Co-authored-by: Exilliar <43271984+Exilliar@users.noreply.github.com>
    
    Co-authored-by: Exilliar <43271984+Exilliar@users.noreply.github.com>
    
    * code pieces and yml files for building infrastructure and deploying to that bucket
    
    * fixed indentation error on publish-staging
    
    * changed when to trigger Build Infrastructure and renamed
    
    Co-authored-by: Exilliar <43271984+Exilliar@users.noreply.github.com>
    
    Co-authored-by: Exilliar <43271984+Exilliar@users.noreply.github.com>

---
+++ infra/staging.tfvars
@@ -0,0 +1,5 @@
+azure_app_client_id="a9bda07c-7f18-4bc3-bfa0-4d1a9df25ef5"
+redirect_uri="https://act.staging-speckle.arup.com/_callback"
+project_name="a-carbon-tool"
+environment = "dev"
+domain = "staging-speckle.arup.com"
---
+++ infra/variables.tf
@@ -0,0 +1,40 @@
+variable "azure_app_client_secret" {
+  type = string
+  description = "Secret for Azure App Registration"
+}
+
+variable "azure_app_client_id" {
+  type = string
+  description = "Unique ID for Azure App Registration"
+}
+
+variable "private_key" {
+  type = string
+  description = "Private RSA key"
+}
+
+variable "public_key" {
+  type = string
+  description = "Public RSA key"
+}
+
+variable "redirect_uri" {
+  type = string
+  description = "Where to redirect traffic after authentication"
+}
+
+variable "project_name" {
+  type = string
+  description = "Identifier for this project"
+}
+
+variable "environment" {
+  type = string
+  description = "dev, staging, prod etc"
+  default = "staging"
+}
+
+variable "domain" {
+  type = string
+  description = "base domain already configured in route 53"
+}

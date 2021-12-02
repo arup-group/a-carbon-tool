2e9f1370f3d997d6fc4d8210dda9582c228d7052
ninehundred <daniel.odowd@arup.com>
2021-12-02 22:51:02 +0000

    add buttons to login to hide arup server url - incomplete - final commit for the evening

--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -2,7 +2,31 @@
   <v-form ref="form" @submit.prevent="checkSubmit">
     <v-container class="d-flex justify-center align-center">
       <v-card style="width: 50%">
-        <v-card-title class="">Log In</v-card-title>
+        <v-card-title class="">Log In here</v-card-title>
+        <!-- added some text to show the user what to do -->
+        <v-card-subtitle class="text--primary"> 
+          Please let us know where you belong!
+        </v-card-subtitle>
+        <div id="arup-xyz">
+          <!-- added button to direct to arup server -->
+          <v-btn name="arup_btn" @click="setServerType($event)" color="secondary" text>
+            Arup Servers
+          </v-btn>
+          <!-- added button to direct to some other server function -->
+          <v-btn name="xyz_btn" @click="setServerType($event)" color="secondary" text>
+            XYZ Servers
+          </v-btn>
+        </div>
+        <!-- updated the text slightly to be more descriptive of action -->
+
+        <!-- TODO: the below should be conditionally rendered -->
+        <template v-if="serverType">
+          <label>arup server</label>
+        </template>
+        <template v-else>
+          <label>xyz server</label>
+        </template>
+
         <v-card-subtitle class="text--primary">
           Define the Speckle server you wish to connect to
         </v-card-subtitle>
@@ -10,7 +34,7 @@
           <v-select
             v-model="model"
             :items="items"
-            label="Select server"
+            label="Select server location"
           ></v-select>
         </v-card-text>
         <v-card-actions class="d-flex justify-end">
@@ -31,13 +55,26 @@ export default class LoginCard extends Vue {
   @Prop() servers!: Server[];
   model: Server = {} as Server;
 
+  serverType = true;
+
+  setServerType(event: any) {
+    console.log(event);
+    console.log(event.target.nodeName);
+    // could do an if span get target parent and then name
+    // then we'd say if the name was arup show one thing if not show the other
+    this.serverType = !this.serverType;
+    return this.serverType;
+    //console.dir(this.serverType);
+  };
+ 
+
   get items() {
     if (this.servers) {
-      return this.servers.map((s) => {
+      return this.servers.map((server) => {// updated s to server to be more descriptive
         return {
-          text: `${s.region} - ${s.url}`,
-          value: s
-        }
+          text: `${server.region}`, //removed  - ${s.url} as we didnt want the user seeing url
+          value: server,
+        };
       });
     } else {
       return [];

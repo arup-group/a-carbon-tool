e25f7f0ae7b5cdb3bb5da054d9d9cdcd8f5d187a
Exilliar <rorywebber1000@gmail.com>
2021-12-02 11:12:57 +0000

    started making thing

---
+++ src/components/Renderer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="renderer"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Viewer as any } from "@speckle/viewer";
+
+@Component
+export default class extends Vue {
+  domElement!: any;
+  mounted() {
+    let renderDomElment = document.getElementById("renderer");
+
+    this.domElement = renderDomElment;
+    this.domElement.style.display = 'inline-block';
+    if (this.$refs.rednererparent) {
+      this.$refs.rendererparent.appendChild(renderDomElement)
+    }
+  }
+}
+</script>

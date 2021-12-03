dc37c2e617d109962f892e423b26699cfa88901c
Katharyn Church <katharyn.church@N-EML1SFML85.local>
2021-12-03 17:22:23 +0000

    menu1b text field setup

---
+++ src/components/Menu1b.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-form ref="form" @submit.prevent="checkSubmit">
+    <v-container class="d-flex justify-center align-center">
+      <v-card style="width: 80%">
+        <v-card-title class="">New Assessment</v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="speckleStream"
+            label="Speckle Stream"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="project"
+            label="Project"
+            required
+          ></v-text-field>
+          <v-select
+            v-model="component"
+            :items="items"
+            label="component"
+          ></v-select>
+          <v-text-field
+            v-model="projectValue"
+            label="Project Value (£)"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="gia"
+            label="Gross Internal Area"
+            required
+          ></v-text-field>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-form>
+</template>

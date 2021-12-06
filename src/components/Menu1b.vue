31285d5c4ccec1449fa9c662bac9486f9ca2f920
Katharyn Church <katharyn.church@N-EML1SFML85.local>
2021-12-06 16:15:15 +0000

    assessment page loads basic form

--- src/components/Menu1b.vue
+++ src/components/Menu1b.vue
@@ -34,3 +34,9 @@
     </v-container>
   </v-form>
 </template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Menu1b extends Vue {}
+</script>
--- src/views/Assessment.vue
+++ src/views/Assessment.vue
@@ -1,11 +1,17 @@
 <template>
-  <p>Assessment working!</p>
+  <v-main>
+    <Menu1b/>
+  </v-main>
 </template>
 
 <script lang="ts">
+import Menu1b from "@/components/Menu1b.vue";
+
 import { Component, Vue } from "vue-property-decorator";
 
-@Component
+@Component({
+  components: { Menu1b }
+  })
 export default class Assessment extends Vue {}
 </script>
 

9c13321d70dd34fa129688154a0600b6fc7ceb0c
Exilliar <rorywebber1000@gmail.com>
2021-12-15 11:38:40 +0000

    fixed conflict in test file

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,6 +1,5 @@
 import { shallowMount } from "@vue/test-utils";
 
-<<<<<<< HEAD
 // describe("HelloWorld.vue", () => {
 //   it("renders props.msg when passed", () => {
 //     const msg = "new message";
@@ -16,14 +15,3 @@ describe("basic", () => {
     expect(true).toBe(true);
   })
 })
-=======
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    //expect(wrapper.text()).toMatch(msg);
-  });
-});
->>>>>>> b1605c8375f9ebceaf6f146fa0c17aa65637adad

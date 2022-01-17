8237fbdba911aa58ba60665f4e407437acd8a562
Tom Bunn <tom.bunn@arup.com>
2022-01-17 14:11:19 +0000

    fixed getter and functions for page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,8 +1,14 @@
 <template>
   <v-main class="page">
-    <v-row>
+    <v-row justify='center'>
       <v-col cols='12'>
-        <v-card flat class="titles mt-6">
+        <v-card flat>
+          <v-img src="/assets/logo.svg" height='80px' class='ma-4' contain>
+          </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols='12'>
+        <v-card flat class="mt-6 mx-4 justify-center">
           <v-card-title class="text-h4">
             Welcome to act | a carbon tool
           </v-card-title>
@@ -43,9 +49,15 @@
               cols='12'
               md='6'
               lg='4'
+              v-if='page===1'
             >
               <v-container class='ma-0 pa-0' fluid>
-                <v-sheet outlined color='primary' rounded class='my-1'>
+                <v-sheet 
+                  outlined
+                  color='primary'
+                  rounded
+                  class='my-1'
+                >
                   <v-card flat height='600' class='d-flex align-center justify-center'>
                     <v-card-actions class='justify-center'>
                       <v-btn x-large outlined color='primary' @click="$router.push('/assessment')">
@@ -107,145 +119,135 @@ import { Vue, Component } from "vue-property-decorator";
 import ProjectCard from "../components/ProjectCard.vue";
 import { Project } from "@/models/project";
 
+@Component ({ components: {ProjectCard} })
 
-export default {
-  components: { ProjectCard },
-  data () {
-    return {
-      itemsPerPage: 9,
-      search:'',
-      filter: {},
-      sortdesc: false,
-      page: 1,
-      keys: [
-        'title',
-        'category'
-      ],
-      projects: [
-        {
-          title: "Super great project",
-          id: 1,
-          co2Values: [
-            {
-              label: "some value 1",
-              value: 50,
-            },
-            {
-              label: "some value 2",
-              value: 20,
-            },
-            {
-              label: "some value 3",
-              value: 10,
-            },
-          ],
-          link: "",
-          category: "Superstructure",
-        },
-        {
-          title: "Imaginary project 3",
-          id: 2,
-          co2Values: [
-            {
-              label: "some value 1",
-              value: 50,
-            },
-            {
-              label: "some value 2",
-              value: 20,
-            },
-            {
-              label: "some value 3",
-              value: 10,
-            },
-          ],
-          link: "",
-          category: "Substructure",
-        },
-        {
-          title: "Some other project",
-          id: 3,
-          co2Values: [
-            {
-              label: "some value 1",
-              value: 50,
-            },
-            {
-              label: "some value 2",
-              value: 20,
-            },
-            {
-              label: "some value 3",
-              value: 10,
-            },
-          ],
-          link: "",
-          category: "Services",
-        },
-        {
-          title: "Some other project",
-          id: 4,
-          co2Values: [
-            {
-              label: "some value 1",
-              value: 50,
-            },
-            {
-              label: "some value 2",
-              value: 20,
-            },
-            {
-              label: "some value 3",
-              value: 10,
-            },
-          ],
-          link: "",
-          category: "Services",
-        },
-        {
-          title: "Some other project",
-          id: 5,
-          co2Values: [
-            {
-              label: "some value 1",
-              value: 50,
-            },
-            {
-              label: "some value 2",
-              value: 20,
-            },
-            {
-              label: "some value 3",
-              value: 10,
-            },
-          ],
-          link: "",
-          category: "Services",
-        },
-      ],
-    }
-  },
-  computed: {
-    // TODO
-    // - fix number of pages
+export default class Landing extends Vue {
+  
+    itemsPerPage = 8
+    search = ''
+    filter = {}
+    sortdesc = false
+    page = 1
+    keys = [
+      'title',
+      'category'
+    ]
+    projects = [
+      {
+        title: "Super great project",
+        id: 1,
+        co2Values: [
+          {
+            label: "some value 1",
+            value: 50,
+          },
+          {
+            label: "some value 2",
+            value: 20,
+          },
+          {
+            label: "some value 3",
+            value: 10,
+          },
+        ],
+        link: "",
+        category: "Superstructure",
+      },
+      {
+        title: "Imaginary project 3",
+        id: 2,
+        co2Values: [
+          {
+            label: "some value 1",
+            value: 50,
+          },
+          {
+            label: "some value 2",
+            value: 20,
+          },
+          {
+            label: "some value 3",
+            value: 10,
+          },
+        ],
+        link: "",
+        category: "Substructure",
+      },
+      {
+        title: "Some other project",
+        id: 3,
+        co2Values: [
+          {
+            label: "some value 1",
+            value: 50,
+          },
+          {
+            label: "some value 2",
+            value: 20,
+          },
+          {
+            label: "some value 3",
+            value: 10,
+          },
+        ],
+        link: "",
+        category: "Services",
+      },
+      {
+        title: "Some other project",
+        id: 4,
+        co2Values: [
+          {
+            label: "some value 1",
+            value: 50,
+          },
+          {
+            label: "some value 2",
+            value: 20,
+          },
+          {
+            label: "some value 3",
+            value: 10,
+          },
+        ],
+        link: "",
+        category: "Services",
+      },
+      {
+        title: "Some other project",
+        id: 5,
+        co2Values: [
+          {
+            label: "some value 1",
+            value: 50,
+          },
+          {
+            label: "some value 2",
+            value: 20,
+          },
+          {
+            label: "some value 3",
+            value: 10,
+          },
+        ],
+        link: "",
+        category: "Services",
+      },
+    ]
 
-    numberOfPages () {
-      return 1
-      // const items = this.projects.length
-      // return Math.ceil(items / this.itemsPerPage)
-    }
-  },
-  methods: {
-    // TODO
-    // Get functions working for next page / previous page
+  get numberOfPages () {
+    const items = this.projects.length
+    return Math.ceil(items / this.itemsPerPage)
+  }
 
-    // nextPage: () => {
-    //   if (this.page + 1 <= this.numberOfPages) this.page += 1
-    // },
-    // formerPage: () => {
-    //   if (this.page - 1 >= 1) this.page -= 1
-    // },
-  },
+  nextPage () {
+    if (this.page + 1 <= this.numberOfPages) this.page += 1
+  }
+
+  formerPage () {
+    if (this.page - 1 >= 1) this.page -= 1
+  }
+
 }
 </script>
 <style scoped>

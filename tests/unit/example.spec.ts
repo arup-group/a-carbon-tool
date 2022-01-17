0d6f5611ccc0889db4ac7ada9b5f0b14fffeb636
ninehundred <ninehundred@hotmail.co.uk>
2022-01-17 15:55:42 +0000

    add example update

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,41 +1,7 @@
-import { shallowMount } from "@vue/test-utils";
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6abc5d1e53c277e080e6cd81ea8544ab5c76e962
-=======
->>>>>>> 9c13321d70dd34fa129688154a0600b6fc7ceb0c
-// describe("HelloWorld.vue", () => {
-//   it("renders props.msg when passed", () => {
-//     const msg = "new message";
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg },
-//     });
-//     //expect(wrapper.text()).toMatch(msg);
-//   });
-// });
-
 describe("basic", () => {
   it("passes", () => {
     expect(true).toBe(true);
   })
 })
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    //expect(wrapper.text()).toMatch(msg);
-  });
-});
->>>>>>> b1605c8375f9ebceaf6f146fa0c17aa65637adad
-=======
->>>>>>> 6abc5d1e53c277e080e6cd81ea8544ab5c76e962
-=======
->>>>>>> 9c13321d70dd34fa129688154a0600b6fc7ceb0c
+
+

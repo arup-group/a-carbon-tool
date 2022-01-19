0441ada2cb8070db958f763f356a1ad782c9c38e
Tom Bunn <tom.bunn@arup.com>
2022-01-19 11:18:36 +0000

    Set up about page and added declatation to login

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,34 +1,73 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" md="9">
+      <v-col cols="12">
         <v-card flat>
-          <v-card-title class="text-h4">About</v-card-title>
+          <v-card-title class="text-h4">Declaration</v-card-title>
           <v-card-text>
-            A carbon tool (act) is an open source web application that enables
-            users to estimate embodied carbon equivalent content from Building
-            Information Models (BIM) - using Speckle to solve the challenge of
-            data interoperability in the AEC industry. It is designed to provide
-            quick and efficient feedback during the design process to enable
-            users to identify carbon hotspots and reduce the impact their design
-            has on the built environment.
+            The construction industry currently faces significant challenges in reducing its environmental impact. The built environment is directly responsible for
+            nearly 40% of all carbon emissions, with 10% coming from embodied carbon - the emissions associated with the construction, maintenance and deconstruction
+            (<a href='https://worldgbc.org/news-media/commitment-includes-embodied-carbon' target='_blank'>WGBC, 2021</a>).
             <br /><br />
-            In November 2021 we officially open-sourced this initiative to
-            foster collaboration in solving the most significant challenge the
-            construction industry faces today - how to minimise the envionmental
-            impact of new construction and major interventions. We believe that
-            this platform can aid in this. Carbon emissions are far from the
-            only climate indicator the construction industry should focus it's
-            attention on. Getting them fully understood so that we can measure
-            and manage them towards our Net Zero aspirations is a step in the
-            right direction.
+            As an intitative, we believe that significant action needs to be taken to curb these emissions, with a target of limiting global temperature rise to 1.5C.
+            Construction professionals - architects, engineers, designers, consultants and contractors - with the power to enact maningful change require the right tools to do so.
             <br /><br />
-            We hope that this platform can be a vehicle for change. This version
-            is open to anyone with access to a Speckle server. Instructions on
-            how to deploy your own version of this platform can be found within
-            our git repository. If you would like to contribute to this
-            initiative please contact us through the links provided.
+            A carbon tool, or 'act', is an open-source intitiative to create appropriate tools for the rapid assessment of embodied 
+            carbon emissions within built-environment assets. It is a work in progress intended to evolve over time. It is free to all to use and it is open to anyone who 
+            would like to contribute. Unlike other lifecycle analysis tools, act is intended to be used by designers as they work to provide quick feedback and enable carbon 
+            to be included as a factor in design as it happens.
           </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols='12'>
+        <v-card flat>
+          <v-card-title>Description</v-card-title>
+          <v-card-text>
+            A carbon tool (act) is an open source web application that enables users to estimate embodied carbon equivalent content from Building
+            Information Models (BIMs). It uses <a href='https://speckle.systems/' target='_blank'>Speckle</a> to access BIMs from a several established softwares,
+            including Revit, Rhino and Civil3D. 
+            <br /><br />
+            It is designed to provide quick and efficient feedback during the design process to enable
+            users to identify embodied carbon hotspots and reduce the impact their design has on the built environment.
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols='12'>
+        <v-card flat>
+          <v-card-title>Definitions</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols='12'>
+        <v-card flat>
+          <v-card-title>Methodology</v-card-title>
+          <v-card-text>
+            <strong>act</strong> follows the methodology set out in <a href='https://www.rics.org/globalassets/rics-website/media/news/whole-life-carbon-assessment-for-the--built-environment-november-2017.pdf' target='_blank'>
+            RICS: Whole life carbon assessment for the build environment (2017)</a> and <a href='https://www.en-standard.eu/bs-en-15978-2011-sustainability-of-construction-works-assessment-of-environmental-performance-of-buildings-calculation-method/'
+            target='_blank'>BS EN 15978 (2011)</a>. 
+            <br />
+          </v-card-text>
+          <v-img src='/assets/carbon-stages.svg' height='600' contain></v-img>
+        </v-card>
+      </v-col>
+      <v-col cols='12'>
+        <v-card flat>
+          <v-card-title>Data</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols='12'>
+        <v-card flat>
+          <v-card-title>Speckle</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols='12'>
+        <v-card flat>
+          <v-card-title>Contributing</v-card-title>
+          <v-card-text></v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols='12'>
+        <v-card>
+          <v-card-title>Contributors</v-card-title>
         </v-card>
       </v-col>
     </v-row>
@@ -39,5 +78,7 @@
 import { Vue, Component } from "vue-property-decorator";
 
 @Component({})
-export default class About extends Vue {}
+export default class About extends Vue {
+
+}
 </script>

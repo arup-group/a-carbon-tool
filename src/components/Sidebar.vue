8c4a39b9ff60e5596fffc7c84d0bf216230ce126
Robert Salca <robert.salca@gmail.com>
2022-01-21 18:53:29 +0000

    center dark-mode button

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -19,33 +19,59 @@
         New Assessment
       </arc-menu-item>
       <arc-menu-item @click="$router.push('/landing')">
-        <arc-icon name="home" slot="prefix" size="small" style="margin-right: 7px"></arc-icon>
+        <arc-icon
+          name="home"
+          slot="prefix"
+          size="small"
+          style="margin-right: 7px"
+        ></arc-icon>
         Home
       </arc-menu-item>
       <arc-menu-item @click="$router.push('/about')">
-        <arc-icon name="info" slot="prefix" size="small" style="margin-right: 7px"></arc-icon>
+        <arc-icon
+          name="info"
+          slot="prefix"
+          size="small"
+          style="margin-right: 7px"
+        ></arc-icon>
         About
       </arc-menu-item>
       <arc-menu-item>
-        <arc-icon name="question" slot="prefix" size="small" style="margin-right: 7px"></arc-icon>
+        <arc-icon
+          name="question"
+          slot="prefix"
+          size="small"
+          style="margin-right: 7px"
+        ></arc-icon>
         Help
       </arc-menu-item>
     </arc-menu>
     <arc-menu style="flex-shrink: 6">
       <arc-menu-item>
-        <arc-icon name="settings" slot="prefix" size="medium" style="margin-right: 7px"></arc-icon>
+        <arc-icon
+          name="settings"
+          slot="prefix"
+          size="medium"
+          style="margin-right: 7px"
+        ></arc-icon>
         Settings
       </arc-menu-item>
       <arc-menu-item v-if="li" @click="logout">
-        <arc-icon name="lock-open" slot="prefix" size="medium" style="margin-right: 7px"></arc-icon>Sign
-        out</arc-menu-item
+        <arc-icon
+          name="lock-open"
+          slot="prefix"
+          size="medium"
+          style="margin-right: 7px"
+        ></arc-icon
+        >Sign out</arc-menu-item
       >
-      <arc-menu-item
+      <arc-button
         v-if="li"
-        style="text-align: center"
+        type="tab"
+        style="width: 100%"
         @click="toggleDarkMode"
         >{{ darkModeButtonText }}
-      </arc-menu-item>
+      </arc-button>
     </arc-menu>
   </arc-sidebar>
 </template>

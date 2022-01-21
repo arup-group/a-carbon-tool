68509b3f43bbbabbb0c9963eee61a1a90e872688
Robert Salca <robert.salca@gmail.com>
2022-01-21 11:21:41 +0000

    import arc components

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,44 +1,43 @@
 <template>
   <arc-sidebar slot="side" title="title">
     <arc-menu>
-      <arc-menu-item
-        value="home"
-        aria-disabled="false"
-        role="menuitem"
-        tabindex="0"
-      >
-        <arc-icon name="home" slot="prefix" size="medium"></arc-icon>
+      <arc-menu-item>
+        <arc-icon name="home" slot="prefix" size="small"></arc-icon>
         Home
       </arc-menu-item>
-      <arc-menu-item
-        value="messages"
-        aria-disabled="false"
-        role="menuitem"
-        tabindex="-1"
-      >
-        <arc-icon name="speech" slot="prefix" size="medium"></arc-icon>
-        Messages
+      <arc-menu-item>
+        <arc-icon name="info" slot="prefix" size="small"></arc-icon>
+        About
+      </arc-menu-item>
+      <arc-menu-item>
+        <arc-icon name="question" slot="prefix" size="small"></arc-icon>
+        Help
+      </arc-menu-item>
+      <arc-menu-item>
+        <arc-icon name="speech" slot="prefix" size="small"></arc-icon>
+        New Assessment
       </arc-menu-item>
     </arc-menu>
     <arc-menu>
-      <arc-menu-item
-        value="settings"
-        aria-disabled="false"
-        role="menuitem"
-        tabindex="0"
-      >
+      <arc-menu-item>
         <arc-icon name="settings" slot="prefix" size="medium"></arc-icon>
         Settings
       </arc-menu-item>
+      <arc-menu-item @click="logout">
+        <arc-icon name="settings" slot="prefix" size="medium"></arc-icon
+        >Log</arc-menu-item
+      >
     </arc-menu>
   </arc-sidebar>
 </template>
 
 <script lang="ts">
 import "@arc-web/components/dist/components/button/arc-button.js";
-import "@arc-web/components/dist/components/icon/arc-icon";
-import "@arc-web/components/dist/components/sidebar/arc-sidebar";
-import "@arc-web/components/dist/components/container/arc-container";
+import "@arc-web/components/dist/components/icon/arc-icon.js";
+import "@arc-web/components/dist/components/sidebar/arc-sidebar.js";
+import "@arc-web/components/dist/components/container/arc-container.js";
+import "@arc-web/components/dist/components/menu/arc-menu.js";
+import "@arc-web/components/dist/components/menu-item/arc-menu-item.js";
 
 import { Vue, Component, Prop, Emit } from "vue-property-decorator";
 

52d38cd53124b9645744591a4207be22da1ab9cd
Exilliar <rorywebber1000@gmail.com>
2022-01-24 13:40:41 +0000

    ran prettier in Menu6.vue

--- src/components/assessment/Menu6.vue
+++ src/components/assessment/Menu6.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
-        <p>Menu6 working!</p>
-        <code>
-            {{ report }}
-        </code>
-    </div>
+  <div>
+    <p>Menu6 working!</p>
+    <code>
+      {{ report }}
+    </code>
+  </div>
 </template>
 <script lang="ts">
 import { ReportPassdown } from "@/models/newAssessment";
@@ -12,6 +12,6 @@ import { Vue, Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class Menu6 extends Vue {
-    @Prop() report!: ReportPassdown;
+  @Prop() report!: ReportPassdown;
 }
 </script>

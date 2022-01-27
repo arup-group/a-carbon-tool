18f5eb7dc0c849da861adc01436f817ef9cf483d
Tom Bunn <tom.bunn@arup.com>
2022-01-27 19:51:52 +0000

    updated about and added images

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12">
-        <v-card flat>
+      <v-col cols="12" xl='8'>
+        <v-card flat outlined class="mt-8">
           <v-card-title class="text-h4">Declaration</v-card-title>
           <v-card-text>
             The construction industry currently faces significant challenges in reducing its environmental impact. The built environment is directly responsible for
@@ -17,34 +17,42 @@
             would like to contribute. Unlike other lifecycle analysis tools, act is intended to be used by designers as they work to provide quick feedback and enable carbon 
             to be included as a factor in design as it happens.
           </v-card-text>
+          <v-img src='/assets/globe.png' contain height='480'></v-img>
         </v-card>
       </v-col>
-      <v-col cols='12'>
-        <v-card flat>
-          <v-card-title>Description</v-card-title>
-          <v-card-text>
-            A carbon tool (act) is an open source web application that enables users to estimate embodied carbon equivalent content from Building
-            Information Models (BIMs). It uses <a href='https://speckle.systems/' target='_blank'>Speckle</a> to access BIMs from a several established softwares,
-            including Revit, Rhino and Civil3D. 
-            <br /><br />
-            It is designed to provide quick and efficient feedback during the design process to enable
-            users to identify embodied carbon hotspots and reduce the impact their design has on the built environment.
+      <v-col cols="12" xl='8'>
+        <v-card flat outlined>
+          <v-card-title class="text-h4">Speckle</v-card-title>
+          <v-card-text class='mb-4'>
+            <strong>act</strong> is built on top of Speckle - a cloud based solution for the AEC industry that provides honest interoperability, real time collaboration, 
+            data management, versioning and automation. Speckle allows us to directly plug into BIM projects and store the resulting assessment in an accessible location 
+            which could form part of a wider workflow. But don't take it from us. Watch the video and follow the links below for more information.
           </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols='12'>
-        <v-card flat>
-          <v-card-title>Speckle</v-card-title>
-          <v-card-text>
-            <strong>act</strong> is built on top of <a href='https://speckle.systems' target='blank'>Speckle</a> - a cloud based solution 
-            for the AEC industry that provides honest interoperability, real time collaboration, data management, versioning and automation. 
-            The use of Speckle allows us access to BIM data agnostic of platform used.<br /><br />
-            In order to use <strong>act</strong> you will need to be a registered user of the <a href='https://speckle.xyz/' target='_blank'>Speckle XYZ</a> 
-            server or a member of Arup staff.<br /><br />
-            If you want to know more about Speckle and to get set up using the Speckle XYZ server check out the 
-            <a href='https://speckle.systems' target='blank'>Speckle systems website</a> <br /><br />
-            If you are the owner or administrator of a Speckle 2.0 server and want to register your server on <strong>act</strong> please get in touch.
-          </v-card-text>
+          <v-card-actions class="justify-center">
+            <iframe 
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/B9humiSpHzM"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </v-card-actions>
+          <v-card-actions class="my-4">
+            <v-row dense>
+              <v-col cols='6' align='right'>
+                <v-btn color='primary' outlined src='https://speckle.systems/' target='_blank'>
+                  Speckle Home
+                </v-btn>
+              </v-col>
+              <v-col cols='6' align='left' scr='https://speckle.xyz/' target='_blank'>
+                <v-btn color='secondary' outlined>
+                  Speckle XYZ
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-actions>
         </v-card>
       </v-col>
       <!-- <v-col cols='12'>
@@ -52,9 +60,9 @@
           <v-card-title>Definitions</v-card-title>
         </v-card>
       </v-col> -->
-      <v-col cols='12'>
-        <v-card flat>
-          <v-card-title>Methodology</v-card-title>
+      <v-col cols="12" xl='8'>
+        <v-card flat outlined>
+          <v-card-title class="text-h4">Methodology</v-card-title>
           <v-card-text>
             <strong>act</strong> follows the methodology set out in <a href='https://www.rics.org/globalassets/rics-website/media/news/whole-life-carbon-assessment-for-the--built-environment-november-2017.pdf' target='_blank'>
             RICS: Whole life carbon assessment for the build environment (2017)</a> and <a href='https://www.en-standard.eu/bs-en-15978-2011-sustainability-of-construction-works-assessment-of-environmental-performance-of-buildings-calculation-method/'
@@ -64,7 +72,7 @@
             the carbon emissions associated with material extraction and processing, transport to site (A4), and emissions on site (A5). The calculation methods 
             used for each of these stages are detailed below.
           </v-card-text>
-          <v-img src='/assets/carbon-stages.svg' height='600' contain></v-img>
+          <v-img src='/assets/carbon-stages.svg' height='600' contain class='mb-8'></v-img>
           <v-card-text>
             <div><strong>Product Stage Carbon (A1-A3)</strong></div>
             Product stage carbon is calculated by applying the mass of material by a carbon factor representing the effort and energy expended in it's creation.
@@ -95,53 +103,66 @@
           <v-img src='/assets/a5-site-equation.png' height='320' contain></v-img>
         </v-card>
       </v-col>
-      <v-col cols='12'>
-        <v-card flat>
-          <v-card-title>Data</v-card-title>
+      <v-col cols="12" xl='8'>
+        <v-card flat outlined>
+          <v-card-title class='text-h4'>Data</v-card-title>
           <v-card-text>
             The data within <strong>act</strong> has been segregated by region. Carbon factors have been selected from the following, openly available, 
-            sources...<br /><br />
-            <div><strong>UK:</strong></div>
-            <ul>
-              <li>ICE v3.0b (2019)</li>
-              <li>IStructE - How to calculate embodied carbon (2020)</li>
-            </ul><br />
-            <div><strong>Australia:</strong></div>
-            <ul>
-              <li>coming soon</li>
-            </ul><br />
-            <div><strong>Europe:</strong></div>
-            <ul>
-              <li>coming soon</li>
-            </ul><br />
-            <div><strong>East Asia:</strong></div>
-            <ul>
-              <li>coming soon</li>
-            </ul><br />
+            sources...
           </v-card-text>
+          <v-card-actions class="mx-3 mb-4">
+            <v-row>
+              <v-col v-for='geo in dataSources' :key='geo.name' cols='12'>
+                <div><strong>{{ geo.name }}</strong></div>
+                <div v-for='source in geo.sources' :key='source.name'>
+                  <v-btn outlined small :src='source.url' class='my-1' color='secondary' target="_blank">
+                    {{ source.name }}
+                  </v-btn>
+                </div>
+              </v-col>
+            </v-row>
+          </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols='12'>
-        <v-card flat>
-          <v-card-title>Contributing</v-card-title>
+      <v-col cols="12" xl='8'>
+        <v-card flat outlined>
+          <v-card-title class="text-h4">Contributing</v-card-title>
           <v-card-text>
-            Check out our repository for guidance on how best to contribute - <a href='https://github.com/arup-group/a-carbon-tool'>github</a>
+            <strong>act</strong> is an open-source initiative with a GPL3 licence. Anyone is welcome to contribute.
+            Check out our repository for guidance on how best to get involved - <a href='https://github.com/arup-group/a-carbon-tool'>github</a>
           </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols='12'>
-        <v-card flat>
-          <v-card-title>Contributors</v-card-title>
-          <v-card-text>
-            <ul>
-              <li>Tom Bunn - product manager and occasional front end dev</li>
-              <li>Stam Psarras - tech lead</li>
-              <li>Rory Webber - front end lead</li>
-              <li>Katharyn Church - front end dev</li>
-              <li>Daniel O'Dowd - front end dev</li>
-              <li>Rob Salca - front end dev</li>
-            </ul>
-          </v-card-text>
+          <v-card-title >Contributors</v-card-title>
+            <v-card-actions class="mb-4 mx-4">
+              <v-row style="width:100%">
+                <v-col cols='12' sm="6" md="4" v-for="item in contributors" :key='item.name'>
+                  <v-sheet 
+                    outlined
+                    color='primary'
+                    rounded
+                    class='my-1'
+                    height="100%"
+                  >
+                    <v-card flat height='100%' class='card-outter'>
+                      <v-img :src="item.img" aspect-ratio="1" contain max-height='240px'></v-img>
+                      <v-card-title>{{ item.name }}</v-card-title>
+                      <v-card-subtitle>{{ item.byline }}</v-card-subtitle>
+                      <v-card-actions fill-height class='card-actions'>
+                        <v-btn icon color='primary' class="ma-2" :disabled="item.git===''" :src="item.git">
+                          <v-icon>
+                            mdi-github
+                          </v-icon>
+                        </v-btn>
+                        <v-btn icon color='primary' class="ma-2" :disabled="item.linkedin===''">
+                          <v-icon>
+                            mdi-linkedin
+                          </v-icon>
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-sheet>
+                </v-col>
+              </v-row>
+            </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
@@ -153,6 +174,115 @@ import { Vue, Component } from "vue-property-decorator";
 
 @Component({})
 export default class About extends Vue {
+  contributors = [
+    {
+      name: "Tom Bunn",
+      img: "/assets/contributors/placeholder-2.png",
+      byline: "Product manager @ Arup | environmental sustainability evangalist",
+      git: "https://github.com/tomhebunn",
+      linkedin: "https://www.linkedin.com/in/tom-bunn-8251a45a/"
+    },
+    {
+      name: "Katharyn Church",
+      img: "/assets/contributors/placeholder-1.png",
+      byline: "Transport consultant / software developer @ Arup",
+      git: "https://github.com/kchurch2",
+      linkedin: ""
+    },
+    {
+      name: "Daniel O'Dowd",
+      img: "/assets/contributors/placeholder-2.png",
+      byline: "BIM manager / developer @ Arup",
+      git: "https://github.com/ninehundred",
+      linkedin: ""
+    },
+    {
+      name: "Stam Psarras",
+      img: "/assets/contributors/placeholder-3.png",
+      byline: "Software developer @ Arup",
+      git: "https://github.com/psarras",
+      linkedin: ""
+    },
+    {
+      name: "Rob Salca",
+      img: "/assets/contributors/placeholder-2.png",
+      byline: "Software engineer @ Arup | Sustainability and salsa enthusiast",
+      git: "https://github.com/ro-sa",
+      linkedin: "https://www.linkedin.com/in/robert-salca"
+    },
+    {
+      name: "Rory Webber",
+      img: "/assets/contributors/placeholder-3.png",
+      byline: "Front end dev @ Arup",
+      git: "https://github.com/exilliar",
+      linkedin: ""
+    },
+  ]
 
+  dataSources = [
+    {
+      name: "UK",
+      sources: [
+        {
+          name: "ICE v3.0b (2019)",
+          url: "https://circularecology.com/embodied-carbon-footprint-database.html"
+        },
+        {
+          name: "IStructE - How to calculate embodied carbon (2021)",
+          url: "https://www.istructe.org/resources/guidance/how-to-calculate-embodied-carbon/"
+        },
+        {
+          name: "IStructE - Mass Timber Embodied Carbon Factors (2022)",
+          url: "https://www.istructe.org/resources/blog/mass-timber-embodied-carbon-factors/"
+        },
+      ]
+    },
+    {
+      name: "Australia",
+      sources: [
+        {
+          name: "coming soon",
+          url: ""
+        }
+      ]
+    },
+    {
+      name: "East Asia",
+      sources: [
+        {
+          name: "coming soon",
+          url: ""
+        }
+      ]
+    },
+    {
+      name: "Europe",
+      sources: [
+        {
+          name: "coming soon",
+          url: ""
+        }
+      ]
+    },
+    {
+      name: "India",
+      sources: [
+        {
+          name: "coming soon",
+          url: ""
+        }
+      ]
+    }
+  ]
 }
 </script>
+
+<style>
+.card-outter {
+  padding-bottom: 50px;
+}
+.card-actions {
+  position: absolute;
+  bottom: 0;
+}
+</style>

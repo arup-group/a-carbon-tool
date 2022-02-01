796065b7f82cfc2198283ac00d0adf90d0608bee
Robert Salca <79453579+ro-sa@users.noreply.github.com>
2022-02-01 16:23:06 +0000

    Sidebar (#59)
    * add wireframe of side menu
    
    * update version of arc
    
    * amake icons available to arc
    
    * import arc components
    
    * add links to buttons
    
    * comment out redundant buttons
    
    * remove redundant functionality - logout and buttons
    
    * add username at top of sidebar
    
    * move dark-mode button to side bar
    
    * fix arc-icon margin (manually)
    
    * center dark-mode button
    
    * remove gap between sidebar panels
    
    * add ACT-dark these to edit colour palette
    
    * switch dark to ACT-dark theme
    
    * comment outsettings and help menu items
    
    * sidebar colour change
    
    * overwite v-card colour
    
    * update arc web comp package
    
    * change sidebar component from arc to vuetify
    
    * update side and nav bar
    
    * remove unnecesary mini option
    
    * conditionally render component if logged in
    
    * move act logo from header to sidebar
    
    * fix error when routing to the current route
    
    * remove toggleClipped()
    
    * update to sidebar behaviour
    
    * fix drawer state warning
    
    * remove data() function
    
    * fix background color of cards
    
    * fix home page redirect

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,12 @@
   <v-main>
     <v-row class="justify-center">
       <v-col cols="12">
-        <v-card flat class="justify-center">
+        <v-card
+          flat
+          border
+          class="justify-center"
+          :color="this.$store.state.darkMode ? '#121212' : ''"
+        >
           <v-img src="/assets/logo.svg" max-height="160px" contain class="mt-8">
           </v-img>
           <v-card-title class="text-h4 justify-center">
@@ -22,10 +27,10 @@
         </v-card>
       </v-col>
       <v-col cols="12" md="9" class="d-flex justify-center align-center">
-        <LoginCard :servers="servers" @submit="logIn"/>
+        <LoginCard :servers="servers" @submit="logIn" />
       </v-col>
       <v-col cols="12">
-        <v-card flat>
+        <v-card flat :color="this.$store.state.darkMode ? '#121212' : ''">
           <v-card-actions class="justify-center">
             <v-btn
               icon

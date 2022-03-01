fed71a775fb32e63678842469b7f68a3a6b02acc
Robert Salca <robert.salca@gmail.com>
2022-03-01 14:29:34 +0000

    match height to maximum card

--- src/components/landing/NewAssessmentCard.vue
+++ src/components/landing/NewAssessmentCard.vue
@@ -1,7 +1,19 @@
 <template>
-  <v-container fluid>
-    <v-sheet outlined color="primary" rounded class="my-1">
-      <v-card flat height="650" class="d-flex align-center justify-center">
+  <v-container fluid class="d-flex align-center justify-center">
+    <v-sheet
+      outlined
+      color="primary"
+      rounded
+      class="d-flex align-center justify-center"
+      height="100%"
+      width="100%"
+    >
+      <v-card
+        flat
+        class="d-flex align-center justify-center"
+        height="100%"
+        width="100%"
+      >
         <v-card-actions class="justify-center">
           <v-btn
             x-large

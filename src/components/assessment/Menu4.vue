c8d3b9576a51fb69692363e7067abaab04334ef0
Exilliar <rorywebber1000@gmail.com>
2022-03-22 17:31:21 +0000

    updated text

--- src/components/assessment/Menu4.vue
+++ src/components/assessment/Menu4.vue
@@ -5,7 +5,7 @@
       <v-chip class="text-center">{{ displayVolume }}m<sup>3</sup></v-chip>
     </div>
     <div v-else>
-      <p>No volume props on speckle objects, calculate volume from model?</p>
+      <p><b>No volume properties on speckle objects! Do you want to calculate volume from model?</b></p>
       <p>Model assumed to be in m<sup>3</sup></p>
       <v-btn color="primary" outlined @click="calcVol">Calculate</v-btn>
     </div>

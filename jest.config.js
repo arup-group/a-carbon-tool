435622262e6a26800280646745012ffbdf036728
Exilliar <rorywebber1000@gmail.com>
2022-08-25 14:23:16 +0100

    made action ignore tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
+  testMatch: [
+    "<rootDir>/tests/unit/**/*.spec.(js|jsx|ts|tsx)",
+    "<rootDir>/**/__tests__/*.(js|jsx|ts|tsx)"
+]
 };

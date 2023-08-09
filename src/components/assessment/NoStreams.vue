d53e534d5dd51c9e597541c7704258b1cf122431
Exilliar <rory.webber@arup.com>
2023-08-09 11:34:27 +0100

    updated for pr comments

--- src/components/assessment/NoStreams.vue
+++ src/components/assessment/NoStreams.vue
@@ -1,16 +1,20 @@
 <template>
-  <v-container class="text-center">
-    <p>You have not been assigned to any streams.</p>
-    <p>
-      Please go to your selected server,
-      <a :href="serverUrl" target="_blank">{{ serverUrl }}</a>
-      , and either create a new stream or be added to an existing stream.
-    </p>
-    <p>
-      More info on how to use Speckle can be found at the
-      <a :href="SpeckleDocsUrl" target="_blank">Speckle docs</a>.
-    </p>
-  </v-container>
+  <div class="d-flex justify-center align-center" style="height: 70vh">
+    <v-card width="50%" class="text-center">
+      <v-card-text class="white--text">
+        <p>You have not been assigned to any streams.</p>
+        <p>
+          Please go to your selected server,
+          <a :href="serverUrl" target="_blank">{{ serverUrl }}</a>
+          , and either create a new stream or be added to an existing one.
+        </p>
+        <p>
+          For more information on how to use Speckle, please visit the
+          <a :href="SpeckleDocsUrl" target="_blank">Speckle docs</a>.
+        </p>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";

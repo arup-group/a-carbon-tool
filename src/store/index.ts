7d97bc81e0f569739db7dee85c2cce9734b87d23
Exilliar <rory.webber@arup.com>
2023-08-31 17:48:22 +0100

    updated version number

--- src/store/index.ts
+++ src/store/index.ts
@@ -53,7 +53,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    version: "0.13.0 \u00DF",
+    version: "0.13.1 \u00DF",
     speckleFolderName: "actcarbonreport",
     speckleViewer: {
       viewer: undefined,
